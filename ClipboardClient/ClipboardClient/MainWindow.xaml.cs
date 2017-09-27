60c38341d2eee4d34f7619f38359651b67f94375
Bailey Miller <bmiller@susquehannasoftware.com>
2017-09-27 16:27:05 -0400

    Removed green underline for potentially bad good. The code doesn't need to be waited for, that is the whole point of threading it.

--- ClipboardClient/ClipboardClient/MainWindow.xaml.cs
+++ ClipboardClient/ClipboardClient/MainWindow.xaml.cs
@@ -69,9 +69,11 @@ namespace ClipboardClient
             ClipboardSyncAgent.OnClipBoardBroadcastRecieved += OnClipBoardBroadcastRecieved;
             ClipboardSyncAgent.OnConnectionStatusChanged += OnConnectionStatusChanged;
 
-            ClipboardSyncAgent.ConnectToServer();
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            ClipboardSyncAgent.ConnectToServer(); // That is all good don't worry
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 
-            
+
 
             ClipboardTicker = new DispatcherTimer(DispatcherPriority.Normal);
             ClipboardTicker.Interval = TimeSpan.FromMilliseconds(500);
